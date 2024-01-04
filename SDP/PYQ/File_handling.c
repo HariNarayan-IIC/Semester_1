ac822cc9c2c038a76edcc24badb4ee125b40d7b7
R Hari Narayan <harinarayan2002@gmail.com>
2024-01-04 05:49:14 +0530

    File_handling

---
+++ SDP/PYQ/File_handling.c
@@ -0,0 +1,28 @@
+//WAP to open a file name 'abc.txt' contains some text. Read content from 'abc.txt'
+//and save its text in another file named “xyz.txt” in reverse.
+// Input: This is practical of software design & programming
+// Output: programming & design software of practical is This
+
+#include <stdio.h>
+
+FILE *fptr1, *fptr2;
+
+void reverse(){
+    char myString[100];
+    if (fscanf(fptr1, "%s", myString)!= 1){
+        return;
+    }
+    reverse();
+    fprintf(fptr2,"%s ", myString);
+}
+
+int main(){
+    fptr1= fopen("abc.txt", "r");
+    fptr2= fopen("xyz.txt", "w");
+    
+    reverse();
+    
+    fclose(fptr1);
+    fclose(fptr2);
+    return 0;
+}

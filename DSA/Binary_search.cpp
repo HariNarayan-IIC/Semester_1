7f9dd028abd59ef2613b879630c9e6f0d9d27b65
R Hari Narayan <harinarayan2002@gmail.com>
2024-01-08 07:06:06 +0530

    Bug removed

--- DSA/Binary_search.cpp
+++ DSA/Binary_search.cpp
@@ -10,11 +10,11 @@ int binSearch(int* arr, int l, int r, int key){
     int mid= (l+r)/2;
     if (key== arr[mid]) return mid;
     else if (key> arr[mid]) binSearch(arr, mid+1, r, key);
-    else binSearch(arr, l, mid, key);
+    else binSearch(arr, l, mid-1, key);
 }
 
 int main(){
     int arr[]= {1,3,5,7,9};
-    cout<< binSearch(arr, 0, 4, 9)<< endl;
+    cout<< binSearch(arr, 0, 4, 1)<< endl;
     return 0;
 }

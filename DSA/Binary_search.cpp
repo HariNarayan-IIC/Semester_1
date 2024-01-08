e49133b97dd14ea8e3f07fb48f9d3f3c9335381d
R Hari Narayan <harinarayan2002@gmail.com>
2024-01-08 06:54:55 +0530

    Binary search

---
+++ DSA/Binary_search.cpp
@@ -0,0 +1,20 @@
+//Binary Search
+
+#include <iostream>
+using namespace std;
+
+int binSearch(int* arr, int l, int r, int key){
+    if (l> r){
+        return -1;
+    }
+    int mid= (l+r)/2;
+    if (key== arr[mid]) return mid;
+    else if (key> arr[mid]) binSearch(arr, mid+1, r, key);
+    else binSearch(arr, l, mid, key);
+}
+
+int main(){
+    int arr[]= {1,3,5,7,9};
+    cout<< binSearch(arr, 0, 4, 9)<< endl;
+    return 0;
+}
